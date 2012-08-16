2b35eb57246d3cc5a3d313b14eb6f315e82e9bb1
Regiden <regiden@hotmail.de>
2012-08-16 18:30:25 +0200

    - added BasicGame.java ( simple implementation of the Game Interface)

---
+++ _RadicalFishGDX/src/de/radicalfish/BasicGame.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2012, Stefan Lange
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Stefan Lange nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package de.radicalfish;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import de.radicalfish.util.RadicalFishException;
+
+/**
+ * Basic Implementation of the {@link Game} Interface. It implements the {@link InputProcessor}. Simply override one of
+ * the methods to supply your own code.
+ * 
+ * @author Stefan Lange
+ * @version 1.0.0
+ * @since 16.08.2012
+ */
+public abstract class BasicGame implements Game, InputProcessor {
+	
+	// GAME METHODS
+	// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+	public abstract void init(GameContainer container) throws RadicalFishException;
+	public abstract void update(GameContainer container, float delta) throws RadicalFishException;
+	public abstract void render(GameContainer container, Graphics g, SpriteBatch batch) throws RadicalFishException;
+	
+	public abstract void pause(GameContainer container) throws RadicalFishException;
+	public abstract void resume(GameContainer container) throws RadicalFishException;
+	
+	public abstract void dispose();
+	
+	// INPUT
+	// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+	public boolean keyTyped(char character) {
+		return false;
+	}
+	public boolean touchDown(int x, int y, int pointer, int button) {
+		return false;
+	}
+	public boolean touchUp(int x, int y, int pointer, int button) {
+		return false;
+	}
+	public boolean touchDragged(int x, int y, int pointer) {
+		return false;
+	}
+	public boolean touchMoved(int x, int y) {
+		return false;
+	}
+	public boolean scrolled(int amount) {
+		return false;
+	}
+	
+}
