ddb2a628d20dec7bf9e35c549ad6bde7a06f299c
Regiden <regiden@hotmail.de>
2012-09-08 19:32:42 +0200

    Added Assets.java class for loading assets via AssetManager.
     - Added the first 2 loaders (SpriteSheetLoader.java and FontSheetLoader.java)
     - Added some handy methods to the class
     - Changed the context to use Assets and not the removed Resource class

---
+++ _RadicalFishGDX/src/de/radicalfish/assets/Assets.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2012, Stefan Lange
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Stefan Lange nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package de.radicalfish.assets;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.AssetLoader;
+import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Logger;
+import de.radicalfish.SpriteSheet;
+import de.radicalfish.assets.SpriteSheetLoader.SpriteSheetParameter;
+import de.radicalfish.font.FontSheet;
+
+/**
+ * Class for loading assets. This is basically the {@link AssetManager} with added {@link AssetLoader}.
+ * <hr>
+ * List of extra class loaders:
+ * <li>{@link SpriteSheetLoader} : needs a {@link SpriteSheetParameter} when loading.</li>
+ * <li>{@link SpriteSheetLoader} : needs a {@link SpriteSheetParameter} when loading.</li>
+ * <hr>
+ * Additionally there are methods to create unmanaged {@link Music} or {@link Sound} instances and switch logging off or
+ * on.
+ * 
+ * @author Stefan Lange
+ * @version 0.2.0
+ * @since 08.09.2012
+ */
+public class Assets extends AssetManager {
+	
+	private boolean done;
+	
+	public Assets() {
+		this(new InternalFileHandleResolver());
+	}
+	public Assets(FileHandleResolver resolver) {
+		super(resolver);
+		setLoader(SpriteSheet.class, new SpriteSheetLoader(resolver));
+		setLoader(FontSheet.class, new FontSheetLoader(resolver));
+		done = false;
+	}
+	
+	// METHODS
+	// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+	public synchronized boolean update() {
+		boolean done = false;
+		if (super.update()) {
+			done = true;
+		}
+		if (done) {
+			this.done = done;
+		}
+		return done;
+	}
+	
+	/**
+	 * @return a new unmanaged {@link Sound} object.
+	 */
+	public Sound createSound(String path) {
+		return createSound(Gdx.files.internal(path));
+	}
+	/**
+	 * @return a new unmanaged {@link Sound} object.
+	 */
+	public Sound createSound(FileHandle handle) {
+		return Gdx.audio.newSound(handle);
+	}
+	/**
+	 * @return a new unmanaged {@link Music} object.
+	 */
+	public Music createMusic(String path) {
+		return createMusic(Gdx.files.internal(path));
+	}
+	/**
+	 * @return a new unmanaged {@link Music} object.
+	 */
+	public Music createMusic(FileHandle handle) {
+		return Gdx.audio.newMusic(handle);
+	}
+	
+	// SETTER
+	// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+	/**
+	 * True if we want logging or not.
+	 */
+	public void setLogging(boolean logging) {
+		if (!logging) {
+			getLogger().setLevel(Logger.NONE);
+		} else {
+			getLogger().setLevel(Logger.DEBUG);
+		}
+	}
+	// GETTER
+	// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+	/**
+	 * @return true if the loading is done. This will be set to true if the {@link AssetManager#update()} returns true
+	 *         (if you unload content you must call to update the done flag).
+	 */
+	public boolean isDone() {
+		return done;
+	}
+}
---
+++ _RadicalFishGDX/src/de/radicalfish/assets/SpriteSheetLoader.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2011, Stefan Lange
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Stefan Lange nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package de.radicalfish.assets;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetLoaderParameters;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+import com.badlogic.gdx.assets.loaders.SynchronousAssetLoader;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
+import de.radicalfish.SpriteSheet;
+import de.radicalfish.assets.SpriteSheetLoader.SpriteSheetParameter;
+
+/**
+ * A Loader for {@link SpriteSheet}s. Passing a Parameter specifies the width and height of a tile. Meaning you MUST
+ * pass this information.
+ * <p>
+ * You can name the SpriteSheet like you want, the dependency will be loaded if needed. it must be set in the parameters.
+ */
+public class SpriteSheetLoader extends SynchronousAssetLoader<SpriteSheet, SpriteSheetParameter> {
+	
+	public SpriteSheetLoader(FileHandleResolver resolver) {
+		super(resolver);
+	}
+	
+	// METHODS
+	// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+	@Override
+	public SpriteSheet load(AssetManager assetManager, String fileName, SpriteSheetParameter parameter) {
+		Texture tex = assetManager.get(parameter.texpath, Texture.class);
+		return new SpriteSheet(tex, parameter.tileWidth, parameter.tileHeight, parameter.cached);
+	}
+	public Array<AssetDescriptor> getDependencies(String fileName, SpriteSheetParameter parameter) {
+		if (parameter == null)
+			throw new IllegalArgumentException("Missing SpriteSheetParameter: " + fileName);
+		
+		Array<AssetDescriptor> deps = new Array<AssetDescriptor>();
+		deps.add(new AssetDescriptor(parameter.texpath, Texture.class));
+		return deps;
+	}
+	
+	public static class SpriteSheetParameter extends AssetLoaderParameters<SpriteSheet> {
+		/** The path to the texture which backs the {@link SpriteSheet}. */
+		public String texpath;
+		/** the width of a single tile. */
+		public int tileWidth;
+		/** The height of a single tile. */
+		public int tileHeight;
+		/** True if the {@link TextureRegion}s should be cached. */
+		public boolean cached;
+		
+		public SpriteSheetParameter(String texpath, int tileWidth, int tileHeight) {
+			this(texpath, tileWidth, tileHeight, true);
+		}
+		public SpriteSheetParameter(String texpath, int tileWidth, int tileHeight, boolean cached) {
+			this.texpath = texpath;
+			this.tileWidth = tileWidth;
+			this.tileHeight = tileHeight;
+			this.cached = cached;
+		}
+		
+	}
+	
+}
